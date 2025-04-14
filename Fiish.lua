6dd52c532239caab294a15d244267f80971b62e1
ELPAPIMC <wikindley@gmail.com>
2025-04-14 00:45:22 -0400

    Update Fiish.lua

--- Fiish.lua
+++ Fiish.lua
@@ -1,1 +1,17 @@
-print("Hi")
+-- Fiish.lua (script para Fiish)
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+
+local Window = Rayfield:CreateWindow({
+    Name = "Soon - Moon: Fiish",
+    LoadingTitle = "Cargando Fiish...",
+    LoadingSubtitle = "by Wikindley",
+})
+
+local Tab = Window:CreateTab("Principal", nil)
+Tab:CreateButton({
+    Name = "Activar Función Principal",
+    Callback = function()
+        print("Función principal para Fiish activada")
+        -- Lógica específica para Fiish (necesito detalles)
+    end,
+})
